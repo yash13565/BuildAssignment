5c7fc00273591886d7006f00c3f426f187989ac5
Yashwardhan Jaiswal <jaiswalyashwardhan20@gmail.com>
2023-04-17 22:29:01 +0530

    created modular css

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import style from './Navbar.module.css'
 function Navbar() {
   return (
     <div>Navbar</div>
---
+++ src/Pages/Contact/Contact.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import style from './Contact.module.css'
+function Contact() {
+  return (
+    <div>Contact</div>
+  )
+}
+
+export default Contact
